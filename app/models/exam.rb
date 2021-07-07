627348e46d8f0b81314ab424fd5dacfb4b0e7227
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-07-07 15:20:43 -0400

    End drive

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,15 @@
 class Exam < ActiveRecord::Base
     has_many :student_exams
     has_many :students, through: :student_exams
+
+    def insert_feedback(student, grade, comment)
+        new_student_exam = StudentExam.new(grade: grade , teacher_comment: comment)
+        new_student_exam.student = student
+        new_student_exam.exam = self
+        new_student_exam.save
+    end
+
+    def class_average
+        self.student_exams.all.average(:grade)
+    end
 end
--- app/models/studentexam.rb
+++ app/models/studentexam.rb
@@ -2,4 +2,20 @@ class StudentExam < ActiveRecord::Base
     belongs_to :student
     belongs_to :exam
 
+
+    def print_details
+       exam = self.exam.subject
+       student = self.student.name
+       grade = self.grade
+       "#{student} took the #{exam} exam receiving a score of #{grade}"
+    end
+
+
+    def  questions_correct_ratio
+        grade = self.grade
+        questions = self.exam.total_questions
+        result = grade / questions
+        "#{result} questions correct out of #{questions} questions total"
+    end
+
 end
